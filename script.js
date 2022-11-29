891ea18577d97f63231ee93636dcb1822df27f8f
ckocsal <116661724+ckocsal@users.noreply.github.com>
2022-11-30 00:58:46 +0300

    ODEVLER.JS

---
+++ script.js
@@ -0,0 +1,61 @@
+//
+var a=5;
+var b=10;
+var c=2;
+
+a=a*a;
+console.log(a+b+c);
+b=b*b;
+console.log(a+b+c);
+c=c*c;
+console.log(a+b+c);
+
+//
+
+var sayilar=[1,5,2,8,3];
+console.log(sayilar);
+sayilar=sayilar.slice(0,3);
+console.log(sayilar);
+
+//
+var yapılacaklar=["CEREN","SUNA","HİRA","HAYAL","SU"];
+console.log(yapılacaklar);
+yapılacaklar.push("ÇAĞATAY");
+console.log(yapılacaklar);
+//
+
+var no = "+905437331470";
+console.log(no.replace("+90", ""));
+
+//
+var yapılacaklar = "Çağatay";
+console.log(yapılacaklar.toUpperCase());
+
+//
+console.log(yapılacaklar.replaceAll("a", "x"));
+//
+
+var number2 = 3;
+var number3 = 55;
+var number4 = 1;
+var number5 = 2;
+var number6 = 3;
+var number7 = number2 + number3 + number4 + number5 + number6;
+var number8 = number7 / 5;
+console.log(number8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
